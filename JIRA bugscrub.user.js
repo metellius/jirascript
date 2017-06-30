92debd94e91fb73c3d7e60b82fb85a22c6cda886
Harald Hvaal <hhvaal@cisco.com>
2017-06-30 12:32:20 +0200

    new version

--- JIRA bugscrub.user.js
+++ JIRA bugscrub.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JIRA bugscrub
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
+// @version      0.3.2
 // @description  go go jira
 // @author       Harald Hvaal
 // @match        https://jira-eng-gpk2.cisco.com/jira/*
@@ -13,6 +13,14 @@
 (function() {
     'use strict';
     var navbar = $('.aui-nav')[1];
+    var state;
+    var timeSpentOnCurrentBug = 0;
+    var overtime = false;
+    var borderVisible = false;
+    var secsPerBug = GM_getValue("secsPerBug", 60);
+    var startBtn = $('<li><a id="start_bugscrub" class="aui-button aui-button-primary aui-style "></a></li>');
+    var tickInterval = 500;
+    var stayQuietForBug = "";
 
     function currentBug() {
         var title = window.document.title;
@@ -37,29 +45,115 @@
         GM_setValue("bugscrubstate", JSON.stringify(s));
     }
 
-    function friendlyTime(secs) {
+    function friendlyTime(ms) {
+        var secs = Math.floor(ms / 1000);
         var modulo = secs % 60;
-        var str = modulo + (modulo == 1 ? " second" : " seconds");
+        var str = modulo + (modulo == 1 ? " sec" : " secs");
         var mins = Math.floor(secs / 60);
         if (mins == 0)
             return str;
         if (mins == 1)
-            return "1 minute, " + str;
-        return mins + " minutes, " + str;
+            return "1 min, " + str;
+        return mins + " mins, " + str;
     }
 
-    var state;
-    var timeSpentOnCurrentBug = 0;
-    var overtime = true;
-    var bgHighlight = true;
-    var timePerBug = 60;
-
-    var startBtn = $('<li><a id="start_bugscrub" class="aui-button aui-button-primary aui-style "></a></li>');
     function updateStartBtnText() {
         startBtn.find('.aui-button').text(state.active ? "Stop bugscrub" : "Start bugscrub");
     }
 
-    $('<li id="scrub_state"><span class="onthisbug"></span>         <span class="onbugscrub"></span></li>').prependTo(navbar);
+    function updateNavbarState(s) {
+        var el = $('#scrub_state');
+        if (!el.length) {
+            var container = $('<div/>');
+            el = $('<div id="scrub_state"><span class="state_text"/> <a class="ignore_link">Ignore for this bug</a></div>')
+                .prependTo(container)
+                .css({
+                    display: "inline-block",
+                    backgroundColor: "#ccc",
+                    padding: ".2em",
+                    borderWidth: ".3em",
+                    // borderColor: "transparent",
+                    borderStyle: "solid",
+                    fontSize: "1.1em",
+                    color: "#444"
+                });
+            container
+                .css({
+                    textAlign: "center",
+                    height: 0
+                })
+                .prependTo($('#stalker'));
+            el.find('.ignore_link')
+                .css({
+                    fontSize: ".8em",
+                    cursor: "pointer"
+                })
+                .click(function() {
+                    stayQuietForBug = currentBug();
+                });
+            container.hide();
+            container.fadeIn(1000);
+        }
+        var sum = 0;
+        var count = 0;
+        for (var bug in state.bugs) {
+            if (state.bugs[bug].timeSpent < 15 * 1000)
+                continue;
+            sum += state.bugs[bug].timeSpent;
+            count++;
+        }
+        el.find('.state_text')
+            .text(state.active ?
+                  "This bug: " + friendlyTime(timeSpentOnCurrentBug) +
+                  " (average " + friendlyTime(sum / count) +
+                  ", total " + Math.floor(s.totalTimeSpent / 60 / 1000) + " mins)" : "");
+
+        if (overtime) {
+            if (stayQuietForBug === currentBug()) {
+                borderVisible = false;
+                el.find('.ignore_link').hide();
+            } else {
+                borderVisible = !borderVisible;
+                el.find('.ignore_link').show();
+            }
+            el.css({
+                borderColor: borderVisible ? "orange" : "transparent"
+            });
+        } else {
+            el.find('.ignore_link').hide();
+            el.css({
+                borderColor: "transparent"
+            });
+        }
+    }
+
+    function tick() {
+        state = loadState();
+        if (state.active) {
+            state.totalTimeSpent += tickInterval;
+            var bug = currentBug();
+            if (bug) {
+                if (!state.bugs[bug])
+                    state.bugs[bug] = {
+                        timeSpent: 0
+                    };
+                if (document.hasFocus())
+                    state.bugs[bug].timeSpent += tickInterval;
+                timeSpentOnCurrentBug = state.bugs[bug].timeSpent;
+            }
+            $('#time_per_bug_btn').show();
+            $('#time_per_bug_btn').text("Set time limit (" + secsPerBug + " secs)");
+            $('#scrub_state').show();
+            overtime = timeSpentOnCurrentBug > secsPerBug * 1000;
+            updateNavbarState(state);
+        } else {
+            $('#time_per_bug_btn').hide();
+            $('#scrub_state').hide();
+        }
+        saveState(state);
+        updateStartBtnText();
+    }
+
     startBtn.prependTo(navbar).click(function() {
         state.active = !state.active;
         if (state.active) {
@@ -70,57 +164,15 @@
         saveState(state);
     });
     $('<li><a id="time_per_bug_btn" class="aui-button aui-button-primary aui-style "></a></li>').prependTo(navbar).click(function() {
-        timePerBug = parseInt(prompt("Time per bug in seconds:", timePerBug));
+        var response = parseInt(prompt("Time per bug in seconds:", secsPerBug));
+        if (!window.isNaN(response)) {
+            secsPerBug = response;
+            GM_setValue("secsPerBug", secsPerBug);
+        }
     });
 
-    function updateNavbarState(s) {
-        var el = $('#scrub_state');
-        el.find('.onthisbug').text(state.active ? "Time spent: " + friendlyTime(timeSpentOnCurrentBug) : "");
-        el.find('.onbugscrub').text(state.active ? "(total: " + friendlyTime(s.totalTimeSpent) + ")" : "");
-        $('#scrub_state span').css({position: "relative", top: "10px"});
-    }
-
-    function tick() {
-        state = loadState();
-        if (state.active) {
-            state.totalTimeSpent += 1;
-            var bug = currentBug();
-            if (bug && document.hasFocus()) {
-                if (!state.bugs[bug])
-                    state.bugs[bug] = {
-                        timeSpent: 0
-                    };
-                state.bugs[bug].timeSpent += 1;
-                timeSpentOnCurrentBug = state.bugs[bug].timeSpent;
-            }
-            $('#time_per_bug_btn').show();
-            $('#time_per_bug_btn').text("Set time limit (" + timePerBug + " secs)");
-            $('#scrub_state').show();
-            updateNavbarState(state);
-        } else {
-            $('#time_per_bug_btn').hide();
-            $('#scrub_state').hide();
-        }
-        saveState(state);
-        overtime = state.active && timeSpentOnCurrentBug > timePerBug;
-        updateStartBtnText();
-    }
     tick();
-    setInterval(tick, 1000);
-
-    setInterval(function() {
-        if (overtime) {
-            $("#issue-content").css({backgroundColor: "#fff3cd"});
-            setTimeout(function() {
-                $("#issue-content").css({backgroundColor: "white"});
-            }, 100);
-        }
-    }, 3000);
-
-    setInterval(function() {
-        bgHighlight = overtime ? !bgHighlight : false;
-        $('#scrub_state .onthisbug').css({color: bgHighlight ? "orange" : "white"});
-    }, 500);
+    setInterval(tick, tickInterval);
 
 })();
 
